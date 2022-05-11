e266c7d689b2d44f3f46055edcb6a3876e8f47d2
gatotoadrian <gatotoadrian@gmail.com>
2022-05-11 10:41:02 +0300

    modified files

--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,7 @@ from config import config_options
 from flask_bootstrap import Bootstrap
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
+# from flask_migrate import Migrate
 from flask_mail import Mail
 # from flask_uploads import UploadSet,configure_uploads,IMAGES
 
--- manage.py
+++ manage.py
@@ -1,5 +1,7 @@
 # from multiprocessing import managers
-from flask_migrate import Migrate,MigrateCommand
+# from flask_migrate import Migrate,MigrateCommand
+# from flask_script import Manager, Server
+from flask_migrate import Migrate, MigrateCommand
 from flask_script import Manager, Server
 
 from app import create_app, db
